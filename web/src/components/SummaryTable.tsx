7c79e2d31c61afe294ba729ae82e175e66f23586
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-01-20 21:45:51 -0300

    Implementa preenchimento dias tabela front web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
     return (
         <div className="w-full flex">
@@ -19,16 +22,20 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                
-                {summaryDates.map((date, indice) => {
+                {summaryDates.map(date => {
                     return (
-                        <div key={date.toString()} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-                            <HabitDay />
-                        </div>
+                        <HabitDay key={date.toString()} />
                     )
                 })}
-                <HabitDay />
 
+                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, indice) => {
+                    return (
+                        <div 
+                            key={indice} 
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                        />
+                    )
+                })}
             </div>
 
         </div>
