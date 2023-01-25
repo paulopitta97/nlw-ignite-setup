0e4229b04a6beb142e9cc8f39042829bee28efa9
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-01-24 23:30:41 -0300

    Implementa estado modal open web

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import logoImage from '../assets/logo.svg';
 import { Plus } from 'phosphor-react';
 
 export function Header() {
+
+    // Estado (variáveis monitoradas pelo React)
+    let [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Imperativa vs Declarativa (ex: modal)
+    // React atua como Declarativa...
+    function buttonClicked() {
+        setIsModalOpen(true);
+    }
+
     return (
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
             <img src={logoImage} alt="Habits" />
 
+            {isModalOpen ? <p>oi</p> : null}
+
             <button 
                 type="button"
+                onClick={buttonClicked}
                 className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
                 >
                 <Plus size={20} className="text-violet-500" /> Novo hábito
