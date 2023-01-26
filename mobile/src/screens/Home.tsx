4023ed22737579513a3277a252059215e0c2c7e7
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-01-25 22:03:15 -0300

    Implementa interface do dia mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,9 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDatesToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation();
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -36,6 +40,7 @@ export function Home() {
                         datesFromYearStart.map(date => (
                             <HabitDay 
                                 key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
                             />
                         ))
                     }
