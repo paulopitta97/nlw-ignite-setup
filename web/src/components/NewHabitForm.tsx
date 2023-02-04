120a5db025373b50e017b7cf19b847f2ea4d6711
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-02-03 22:53:58 -0300

    Cria novo hábito pela api http

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from 'react';
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -9,9 +10,21 @@ export function NewHabitForm() {
     const [title, setTitle] = useState('')
     const [weekDays, setWeekDays] = useState<number[]>([])
 
-    function createNewHabit(event: FormEvent) {
+    async function createNewHabit(event: FormEvent) {
         event.preventDefault();
-        console.log(title, weekDays)
+
+        if(!title || weekDays.length === 0)
+            return;
+
+        await api.post('habits', {
+            title,
+            weekDays
+        })
+
+        setTitle('')
+        setWeekDays([])
+
+        alert('Hábito criado com sucesso!')
     }
 
     function handleToggleWeekDay(weekDay: number) {
@@ -37,6 +50,7 @@ export function NewHabitForm() {
                 placeholder="ex.: Exercícios, dormir bem, etc..." 
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
                 autoFocus 
+                value={title}
                 onChange={(event) => setTitle(event.target.value)}
             />
 
@@ -51,6 +65,7 @@ export function NewHabitForm() {
                             key={weekDay} 
                             className='flex items-center gap-3 group'
                             onCheckedChange={() => handleToggleWeekDay(index)}
+                            checked={weekDays.includes(index)}
                             >
                                 <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
                                     <Checkbox.Indicator>
