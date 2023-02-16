0ae110b6448bc3c57015a17e48ff53226406ecc7
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-02-16 00:21:54 -0300

    Altera visuais de habitos cliente web

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import { api } from "../lib/axios";
 
 interface HabitsListProps {
     date: Date;
+    onCompletedChange: (completed: number) => void; 
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
     completedHabits: string[]
 }
 
-export function HabitsList({date}: HabitsListProps) {
+export function HabitsList({date, onCompletedChange}: HabitsListProps) {
 
     const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
@@ -47,6 +48,8 @@ export function HabitsList({date}: HabitsListProps) {
             possibleHabits: habitsInfo!.possibleHabits,
             completedHabits
         })
+
+        onCompletedChange(completedHabits.length)
     }
 
     const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
@@ -58,11 +61,11 @@ export function HabitsList({date}: HabitsListProps) {
                     <Checkbox.Root 
                         key={habit.id} 
                         onCheckedChange={() => handleToggleHabit(habit.id)}
-                        className='flex items-center gap-3 group'
+                        className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
                         checked={habitsInfo.completedHabits.includes(habit.id)}
                         disabled={isDateInPast}
                         >
-                        <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                        <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                             <Checkbox.Indicator>
                                 <Check size={20} className="text-white" />
                             </Checkbox.Indicator>
