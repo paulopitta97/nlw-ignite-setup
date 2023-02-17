b1514dc5b452bfa4298a17f698ee3991e39c54c2
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-02-16 23:52:57 -0300

    Bloqueia edição habitos data passada mobile

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -9,6 +9,7 @@ import { Loading } from '../components/Loading';
 import { api } from '../lib/axios';
 import { generateProgressPercentage } from '../utils/generate-progress-percentage';
 import { HabitsEmpty } from '../components/HabitsEmpty';
+import clsx from 'clsx';
 
 interface Params {
     date: string;
@@ -30,6 +31,7 @@ export function Habit() {
     const route = useRoute();
     const { date } = route.params as Params;
     const parsedDate = dayjs(date);
+    const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
     const dayOfWeek = parsedDate.format('dddd');
     const dayAndMonth = parsedDate.format('DD/MM');
 
@@ -89,7 +91,9 @@ export function Habit() {
 
                 <ProgressBar progress={habitsProgress} />
 
-                <View className='mt-6'>
+                <View className={clsx("mt-6", {
+                    ["opacity-50"]: isDateInPast
+                })}>
                     {
                         dayInfo?.possibleHabits ?
                             dayInfo?.possibleHabits.map(habit => 
@@ -98,12 +102,21 @@ export function Habit() {
                                     title={habit.title}
                                     checked={completedHabits.includes(habit.id)}
                                     onPress={() => handleToggleHabit(habit.id)}
+                                    disabled={isDateInPast}
                                 />
                             )
                             :
                             <HabitsEmpty />
                     }
                 </View>
+
+                {
+                    isDateInPast && (
+                        <Text className='text-white mt-10 text-center'>
+                            Você não pode editar hábitos de uma data passada.
+                        </Text>
+                    )
+                }
 
             </ScrollView>
         </View>
