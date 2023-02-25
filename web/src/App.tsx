6b4f359ab05d579c87096493ad042aa98d334fe5
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2023-02-24 23:18:56 -0300

    Implementa notifications service-worker

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,18 @@ import { SummaryTable } from './components/SummaryTable';
 import './lib/dayjs';
 import './styles/global.css';
 
+// Service Worker para Push Notifications
+navigator.serviceWorker.register('service-worker.js');
+
+// Exemplo simples de notificação (que possui limitações) no navegador
+// window.Notification.requestPermission(permission => {
+//   if(permission === 'granted') {
+//     new window.Notification('Habits', {
+//       body: 'Texto',
+//     });
+//   }
+// })
+
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
